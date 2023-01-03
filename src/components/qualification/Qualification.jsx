84f20a1167dea1800e958a427286206994ad03e6
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-01-04 00:04:09 +0100

    Qualification and Education section

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./qualification.css"
 
 
 const Qualification = () => {
+
+    const [toggleState, setToggleState] = useState (1);
+    const toggleTab = (index) => setToggleState (index);
+
+
     return (
         <section className="qualification section">
             <h2 className="section__title">Qualification</h2>
@@ -11,17 +16,17 @@ const Qualification = () => {
 
             <div className="qualification__container container">
                 <div className="qualification__tabs">
-                    <div className="qualification__button qualification__active button--flex">
-                        <i className="uil uil-graduation-cap qualification__icon"></i> Education
+                    <div className={ toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"} onClick={() => toggleTab(1)}>
+                        <i className="uil uil-graduation-cap qualification__icon"></i> {" "} Education
                     </div>
 
-                    <div className="qualification__button button--flex">
+                    <div className={ toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"} onClick={() => toggleTab(2)}>
                         <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
                     </div>
                 </div>
 
                 <div className="qualification__sections">
-                    <div className="qualification__content">
+                    <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
                         <div className="qualification__data">
                             <div>
                                 <h3 className="qualification__title">Software Engineer</h3>
