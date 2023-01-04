352b82e2c1cef42d5979eec39ed685896e49a159
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-01-05 00:09:49 +0100

    Testimonials section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from "./components/services/Services";
-import Qualification from "./components/qualification/Qualification"
+import Qualification from "./components/qualification/Qualification";
+import Testimonials from "./components/testimonials/Testimonials";
 
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
             <Skills />
             <Services />
             <Qualification />
+            <Testimonials />
         </main>
         </>
     )
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Testimonials = () => {
+    return (
+        <div>Hi</div>
+    )
+}
+
+export default Testimonials;
