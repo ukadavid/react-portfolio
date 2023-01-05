70fadacc1f4e19132aa5e38238a30dfb7ffed0e7
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-01-05 22:55:24 +0100

    Testimonial section v1

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,27 @@
+import image1 from "../../assets/Judexchamp-logo.png";
+import image2 from "../../assets/Kenxedu-logo.png";
+import image3 from "../../assets/Logo-1.png";
+
+
+export const Data = [
+    {
+        id: 1,
+        description: "Tobe did a really good job by not only giving us an online presence but also helping us brainstorm on our business and proposing solutions that help improve profitability by 20%",
+        image: image3,
+        title: "Pison Healthcare"
+    },
+
+    {
+        id: 2,
+        description: "He helped us with a very good website that ranked well on SEO, which impacted my business positively",
+        image: image1,
+        title: "JudexChamp",
+    },
+
+    {
+        id: 3,
+        description: "I have worked with Tobe on over 20 projects and he always delivers quality work",
+        image: image2,
+        title: "Kenxedu",
+    }
+]
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "./testimonials.css"
+import { Data } from "./Data";
 
 
 const Testimonials = () => {
     return (
-        <div>Hi</div>
+        <section className="testimonial container section">
+            <h2 className="section__title">My Clients Say</h2>
+            <span className="section__subtitle">Testimonials</span>
+
+            <div className="Testimonial__container">
+                {Data.map(({id, description, image, title,}) => {
+                    return (
+                        <div className="testimonial__card" key={id}>
+                            <p className="testimonial__description">{description}</p>
+                            <img src={image} alt="" className="testimonial__img" />
+                            <h3 className="testimonial__name">{title}</h3>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>        
     )
 }
 
