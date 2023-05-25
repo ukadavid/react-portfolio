56986dd85299db787200d8a25528eeffac819218
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-05-25 11:43:42 +0100

    update the nav links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">
+        <a href="/" className="nav__logo">
           Tobechukwu Uka
         </a>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
