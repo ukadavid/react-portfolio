e33e7e208692731e2f3a7395d270389eb17fcb67
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-05-26 02:22:08 +0100

    description change done

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -10,7 +10,7 @@ const Data = () => {
                 <img className="home__hand" src={hellopic} alt="my profile" />
             </h1>
             <h3 className="home__subtitle">Software Engineer</h3>
-            <p className="home__description">I'm a creative Full-Stack Engineer with 2 years of experience providing high-impact web solutions for diverse industries.</p>
+            <p className="home__description">I'm a creative Full-Stack Engineer with 2 years of professional experience providing high-impact web solutions for diverse industries.</p>
 
             <a href="#contact" className="button button--flex">
                 Say Hi <i class="button__contact uil uil-envelope-send"></i>
